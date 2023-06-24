95df9211182f9594c3d19b33ea15ad911bf3a432
zorenyikristof <zorenyi.kristof@gmail.com>
2023-06-24 18:00:32 +0200

    initial commit

---
+++ src/main/java/at/technikumwien/slmabschlusspruefung/SlmabschlusspruefungApplication.java
@@ -0,0 +1,13 @@
+package at.technikumwien.slmabschlusspruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmabschlusspruefungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmabschlusspruefungApplication.class, args);
+    }
+
+}
